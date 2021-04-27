002768b40b3f479ed16ef92e071f5e877d753da6
ayush <ayush.gupta.jk@gmail.com>
2021-04-27 12:18:24 +0530

    Created a basic GlobalStyle  👷‍♂️

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,19 @@
+import * as styled from 'styled-components';
+
+export const GlobalStyle = styled.createGlobalStyle`
+	:root {
+	}
+
+	html,
+	body {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+	*,
+	*:before,
+	*:after {
+		padding: 0;
+		margin: 0;
+	}
+`;
