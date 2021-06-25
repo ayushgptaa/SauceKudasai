168fe49a8a76e7c18e82097f6ee9a676b9faa2e4
Ayush <ayush.gupta.jk@gmail.com>
2021-06-26 00:31:21 +0530

    👾 Nav fixed for desktop

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -4,19 +4,23 @@ import styled from 'styled-components';
 const Navbar = styled.nav`
 	padding: 0.8rem;
 	background: var(--nav);
-	display: grid;
-	place-items: center;
+	display: flex;
 	border-radius: 35px;
 	width: 90%;
 	margin: auto;
 	margin-top: 1rem;
 	mix-blend-mode: normal;
 	filter: drop-shadow(0px 4px 10px rgba(0, 0, 0, 0.25));
+
+	@media (min-width: 900px) {
+		margin-top: 3rem;
+		width: 600px;
+	}
 `;
 
 const Title = styled.h1`
-	font-size: 1.1rem;
-	font-weight: var(--semi-bold);
+	font-size: 1.24rem;
+	font-weight: var(--medium);
 	color: #000;
 `;
 
@@ -24,7 +28,7 @@ const index = () => {
 	return (
 		<>
 			<Navbar>
-				<Title>Whatsthesauce</Title>
+				<Title>WhatstheSauce</Title>
 			</Navbar>
 		</>
 	);
