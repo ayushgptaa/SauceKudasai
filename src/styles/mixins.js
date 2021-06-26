20cb8911d932815f8b0be5fd58267d70c3155f72
Ayush <ayush.gupta.jk@gmail.com>
2021-06-26 23:28:03 +0530

    🐛 Added media queries in mixins

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
+import { css } from 'styled-components';
 
 export const Imagecontainer = styled.div`
 	object-fit: cover;
 	border-radius: var(--radius);
 `;
+
+const breakpoints = {
+	xs: '450px',
+	sm: '550px',
+	md: '750px',
+	lg: '900px',
+};
+
+export const respondTo = Object.keys(breakpoints).reduce((accumulator, label) => {
+	accumulator[label] = (...args) => css`
+		@media (min-width: ${breakpoints[label]}) {
+			${css(...args)};
+		}
+	`;
+	return accumulator;
+}, {});
