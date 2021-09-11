1a2edde90eb9244814960f0ee8c4c23aa13a0c42
Ayush <ayush.gupta.jk@gmail.com>
2021-09-11 21:46:56 +0530

    🚀 Added Service wowrker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceworker from './serviceWorkerRegistration';
 
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceworker.register();
